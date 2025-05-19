08018fa5afa25b787915f94f267ac78d0586eba5
Ka-Chun Chau <kachun@kachunchau.com>
2025-05-20 09:42:06 +1000

    chore: fix work image looping

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,1 +1,6 @@
-export default new Map();
+
+import __ASTRO_IMAGE_IMPORT_ZFxCvj from "@images/public-facing-sites@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fpublic-facing-sites.mdx";
+import __ASTRO_IMAGE_IMPORT_S36oF from "@images/settings@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fsettings.mdx";
+import __ASTRO_IMAGE_IMPORT_1Wv22e from "@images/ab-testing@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fab-testing.mdx";
+export default new Map([["@images/public-facing-sites@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fpublic-facing-sites.mdx", __ASTRO_IMAGE_IMPORT_ZFxCvj], ["@images/settings@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fsettings.mdx", __ASTRO_IMAGE_IMPORT_S36oF], ["@images/ab-testing@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fab-testing.mdx", __ASTRO_IMAGE_IMPORT_1Wv22e]]);
+		
--- src/content.config.ts
+++ src/content.config.ts
@@ -3,16 +3,18 @@ import { glob } from "astro/loaders";
 
 const work = defineCollection({
   loader: glob({ pattern: "**/*.{md,mdx}", base: "./src/work" }),
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    headline: z.string(),
-    slug: z.string(),
-    order: z.number().optional(),
-    gradient: z.string().optional(),
-    aligment: z.string().optional(),
-    draft: z.boolean().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      headline: z.string(),
+      slug: z.string(),
+      cover: image(),
+      order: z.number().optional(),
+      gradient: z.string().optional(),
+      alignment: z.string().optional(),
+      draft: z.boolean().optional(),
+    }),
 });
 
 export const collections = { work };
