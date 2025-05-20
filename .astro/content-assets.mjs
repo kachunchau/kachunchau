3a351c0886236d302ecfa369768ecd496d8dd1c7
Ka-Chun Chau <kachun@kachunchau.com>
2025-05-20 17:38:13 +1000

    chore: layout tweaks

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,6 +1,6 @@
 
 import __ASTRO_IMAGE_IMPORT_1Wv22e from "@images/ab-testing@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fab-testing.mdx";
-import __ASTRO_IMAGE_IMPORT_S36oF from "@images/settings@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fsettings.mdx";
 import __ASTRO_IMAGE_IMPORT_ZFxCvj from "@images/public-facing-sites@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fpublic-facing-sites.mdx";
-export default new Map([["@images/ab-testing@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fab-testing.mdx", __ASTRO_IMAGE_IMPORT_1Wv22e], ["@images/settings@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fsettings.mdx", __ASTRO_IMAGE_IMPORT_S36oF], ["@images/public-facing-sites@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fpublic-facing-sites.mdx", __ASTRO_IMAGE_IMPORT_ZFxCvj]]);
+import __ASTRO_IMAGE_IMPORT_S36oF from "@images/settings@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fsettings.mdx";
+export default new Map([["@images/ab-testing@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fab-testing.mdx", __ASTRO_IMAGE_IMPORT_1Wv22e], ["@images/public-facing-sites@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fpublic-facing-sites.mdx", __ASTRO_IMAGE_IMPORT_ZFxCvj], ["@images/settings@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fsettings.mdx", __ASTRO_IMAGE_IMPORT_S36oF]]);
 		
