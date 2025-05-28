d398c7e30afc9c8140e6d0bebacc2d35a109ea98
Ka-Chun Chau <kachun@kachunchau.com>
2025-05-28 17:58:19 +1000

    chore: more air hustle case study tweaks

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,8 +1,7 @@
 
-import __ASTRO_IMAGE_IMPORT_1Wv22e from "@images/ab-testing@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fab-testing.mdx";
-import __ASTRO_IMAGE_IMPORT_ZFxCvj from "@images/public-facing-sites@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fpublic-facing-sites.mdx";
-import __ASTRO_IMAGE_IMPORT_S36oF from "@images/settings@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fsettings.mdx";
-import __ASTRO_IMAGE_IMPORT_Z5ujQG from "@images/splendid-flights@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fsplendid-flights.mdx";
 import __ASTRO_IMAGE_IMPORT_Z2cvXCR from "@images/splendid-flights@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fair-hustle.mdx";
-export default new Map([["@images/ab-testing@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fab-testing.mdx", __ASTRO_IMAGE_IMPORT_1Wv22e], ["@images/public-facing-sites@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fpublic-facing-sites.mdx", __ASTRO_IMAGE_IMPORT_ZFxCvj], ["@images/settings@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fsettings.mdx", __ASTRO_IMAGE_IMPORT_S36oF], ["@images/splendid-flights@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fsplendid-flights.mdx", __ASTRO_IMAGE_IMPORT_Z5ujQG], ["@images/splendid-flights@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fair-hustle.mdx", __ASTRO_IMAGE_IMPORT_Z2cvXCR]]);
+import __ASTRO_IMAGE_IMPORT_1Wv22e from "@images/ab-testing@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fab-testing.mdx";
+import __ASTRO_IMAGE_IMPORT_S36oF from "@images/settings@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fsettings.mdx";
+import __ASTRO_IMAGE_IMPORT_ZFxCvj from "@images/public-facing-sites@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fpublic-facing-sites.mdx";
+export default new Map([["@images/splendid-flights@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fair-hustle.mdx", __ASTRO_IMAGE_IMPORT_Z2cvXCR], ["@images/ab-testing@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fab-testing.mdx", __ASTRO_IMAGE_IMPORT_1Wv22e], ["@images/settings@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fsettings.mdx", __ASTRO_IMAGE_IMPORT_S36oF], ["@images/public-facing-sites@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fpublic-facing-sites.mdx", __ASTRO_IMAGE_IMPORT_ZFxCvj]]);
 		
