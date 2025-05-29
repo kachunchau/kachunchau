0d121d8530b54ef1d2ea71c71db7af407f45cb95
Ka-Chun Chau <kachun@kachunchau.com>
2025-05-29 17:33:58 +1000

    chore: fix netlify deploy issue

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,7 +1,7 @@
 
 import __ASTRO_IMAGE_IMPORT_S36oF from "@images/settings@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fsettings.mdx";
-import __ASTRO_IMAGE_IMPORT_1Wv22e from "@images/ab-testing@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fab-testing.mdx";
 import __ASTRO_IMAGE_IMPORT_Z2cvXCR from "@images/splendid-flights@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fair-hustle.mdx";
+import __ASTRO_IMAGE_IMPORT_1Wv22e from "@images/ab-testing@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fab-testing.mdx";
 import __ASTRO_IMAGE_IMPORT_ZFxCvj from "@images/public-facing-sites@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fpublic-facing-sites.mdx";
-export default new Map([["@images/settings@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fsettings.mdx", __ASTRO_IMAGE_IMPORT_S36oF], ["@images/ab-testing@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fab-testing.mdx", __ASTRO_IMAGE_IMPORT_1Wv22e], ["@images/splendid-flights@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fair-hustle.mdx", __ASTRO_IMAGE_IMPORT_Z2cvXCR], ["@images/public-facing-sites@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fpublic-facing-sites.mdx", __ASTRO_IMAGE_IMPORT_ZFxCvj]]);
+export default new Map([["@images/settings@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fsettings.mdx", __ASTRO_IMAGE_IMPORT_S36oF], ["@images/splendid-flights@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fair-hustle.mdx", __ASTRO_IMAGE_IMPORT_Z2cvXCR], ["@images/ab-testing@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fab-testing.mdx", __ASTRO_IMAGE_IMPORT_1Wv22e], ["@images/public-facing-sites@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fpublic-facing-sites.mdx", __ASTRO_IMAGE_IMPORT_ZFxCvj]]);
 		
