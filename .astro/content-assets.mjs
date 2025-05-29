e82967f1b239c1d68333d32de2686635fc51bb1f
Ka-Chun Chau <kachun@kachunchau.com>
2025-05-29 17:19:14 +1000

    chore: fix result images

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,7 +1,7 @@
 
-import __ASTRO_IMAGE_IMPORT_Z2cvXCR from "@images/splendid-flights@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fair-hustle.mdx";
 import __ASTRO_IMAGE_IMPORT_S36oF from "@images/settings@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fsettings.mdx";
-import __ASTRO_IMAGE_IMPORT_ZFxCvj from "@images/public-facing-sites@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fpublic-facing-sites.mdx";
 import __ASTRO_IMAGE_IMPORT_1Wv22e from "@images/ab-testing@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fab-testing.mdx";
-export default new Map([["@images/splendid-flights@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fair-hustle.mdx", __ASTRO_IMAGE_IMPORT_Z2cvXCR], ["@images/settings@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fsettings.mdx", __ASTRO_IMAGE_IMPORT_S36oF], ["@images/public-facing-sites@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fpublic-facing-sites.mdx", __ASTRO_IMAGE_IMPORT_ZFxCvj], ["@images/ab-testing@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fab-testing.mdx", __ASTRO_IMAGE_IMPORT_1Wv22e]]);
+import __ASTRO_IMAGE_IMPORT_Z2cvXCR from "@images/splendid-flights@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fair-hustle.mdx";
+import __ASTRO_IMAGE_IMPORT_ZFxCvj from "@images/public-facing-sites@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fpublic-facing-sites.mdx";
+export default new Map([["@images/settings@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fsettings.mdx", __ASTRO_IMAGE_IMPORT_S36oF], ["@images/ab-testing@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fab-testing.mdx", __ASTRO_IMAGE_IMPORT_1Wv22e], ["@images/splendid-flights@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fair-hustle.mdx", __ASTRO_IMAGE_IMPORT_Z2cvXCR], ["@images/public-facing-sites@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fpublic-facing-sites.mdx", __ASTRO_IMAGE_IMPORT_ZFxCvj]]);
 		
