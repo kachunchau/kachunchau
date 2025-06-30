abbf531a931868b30468918143a077fc47eafbbe
Ka-Chun Chau <kachun@kachunchau.com>
2025-06-30 15:42:00 +1000

    chore: update work

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,7 +1,7 @@
 
+import __ASTRO_IMAGE_IMPORT_1Wv22e from "@images/ab-testing@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fab-testing.mdx";
 import __ASTRO_IMAGE_IMPORT_1Acu5 from "@images/journeys@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fjourneys.mdx";
 import __ASTRO_IMAGE_IMPORT_Z2cvXCR from "@images/splendid-flights@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fair-hustle.mdx";
 import __ASTRO_IMAGE_IMPORT_S36oF from "@images/settings@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fsettings.mdx";
-import __ASTRO_IMAGE_IMPORT_1Wv22e from "@images/ab-testing@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fab-testing.mdx";
-export default new Map([["@images/journeys@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fjourneys.mdx", __ASTRO_IMAGE_IMPORT_1Acu5], ["@images/splendid-flights@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fair-hustle.mdx", __ASTRO_IMAGE_IMPORT_Z2cvXCR], ["@images/settings@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fsettings.mdx", __ASTRO_IMAGE_IMPORT_S36oF], ["@images/ab-testing@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fab-testing.mdx", __ASTRO_IMAGE_IMPORT_1Wv22e]]);
+export default new Map([["@images/ab-testing@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fab-testing.mdx", __ASTRO_IMAGE_IMPORT_1Wv22e], ["@images/journeys@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fjourneys.mdx", __ASTRO_IMAGE_IMPORT_1Acu5], ["@images/splendid-flights@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fair-hustle.mdx", __ASTRO_IMAGE_IMPORT_Z2cvXCR], ["@images/settings@2x.png?astroContentImageFlag=&importer=src%2Fwork%2Fsettings.mdx", __ASTRO_IMAGE_IMPORT_S36oF]]);
 		
