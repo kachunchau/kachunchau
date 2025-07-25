c638cd7017b6b79052e297a589e80f50b6d76bd3
Ka-Chun Chau <kachun@kachunchau.com>
2025-07-25 19:52:36 +1000

    chore: update hero image code

---
+++ src/components/styleMaps.ts
@@ -0,0 +1,8 @@
+export const gradients: Record<
+  "primary" | "primary-rev" | "secondary",
+  string
+> = {
+  primary: "from-cyan-100 to-yellow-50",
+  "primary-rev": "from-yellow-50 to-cyan-100",
+  secondary: "from-rose-50 to-blue-100",
+};
